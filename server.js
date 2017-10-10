826606ae0814ed9feb5057ab1708d17efa8696eb
Unknown <mingkaichen2009@gmail.com>
2017-10-09 19:12:06 -0700

    res json instead of status send

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const routes = require('./routes');
 routes.get('/lasterror', reporter.lastError);
-routes.get('/reachable', (req, res) => res.status(400).send("success")); // always reachable
+routes.get('/reachable', (req, res) => res.json("success")); // always reachable
 app.use(routes);
 
 // Listen on provided port, on all network interfaces.
