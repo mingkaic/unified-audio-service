1a6d9c8b025ad4ac9f2a24eb019405d0cabc143c
Unknown <mingkaichen2009@gmail.com>
2017-10-09 19:05:39 -0700

    missing status 400 in reachable

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const routes = require('./routes');
 routes.get('/lasterror', reporter.lastError);
-routes.get('/reachable', (req, res) => res.status().send("success")); // always reachable
+routes.get('/reachable', (req, res) => res.status(400).send("success")); // always reachable
 app.use(routes);
 
 // Listen on provided port, on all network interfaces.
