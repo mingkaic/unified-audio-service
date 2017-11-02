6945726911f7757105adea7a3dd9d226a8154963
Unknown <mingkaichen2009@gmail.com>
2017-11-02 16:59:08 -0700

    implemented grpc server (untested)

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-git clone https://github.com/mingkaic/shared_mongodb_api.git database
+# git clone https://github.com/mingkaic/shared_mongodb_api.git database
+# git clone https://github.com/mingkaic/shared_grpc.git grpc
+git submodule update --recursive --remote # grab subprojects
 apt-get update && apt-get install -y ffmpeg
-npm install
+
+npm install;
